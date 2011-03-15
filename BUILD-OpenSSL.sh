ab9a59807783196321032f053a6d5e3f87c432dd
Reid Ellis <reid.ellis@spatialview.com>
2011-03-15 17:18:30 -0400

    Trying to build both debug and release libraries, and creating a universal build of each

--- BUILD-OpenSSL.sh
+++ BUILD-OpenSSL.sh
@@ -9,7 +9,7 @@ build_openssl() {
 	local PLATFORM="$3"
 	local SDKPATH="$4"
 
-	echo "Building binary for iPhone $LIBNAME $PLATFORM to $DISTDIR"
+	p "Building binary for iPhone $LIBNAME $PLATFORM to $DISTDIR" 
 
 	case $LIBNAME in
 		${LIBNAME_DEVICE})  ARCH="armv6";ASSEMBLY="no-asm";;
@@ -18,45 +18,54 @@ build_openssl() {
 
 	cd "${TARGET_DIR}"
 
-	echo Patching crypto/ui/ui_openssl.c
-	echo From: `fgrep 'intr_signal;' crypto/ui/ui_openssl.c`
-	perl -pi.bak \
+	p Patching crypto/ui/ui_openssl.c
+	p From: `fgrep 'intr_signal;' crypto/ui/ui_openssl.c`
+	x perl -pi.bak \
 		-e 's/static volatile sig_atomic_t intr_signal;/static volatile int intr_signal;/;' \
 		crypto/ui/ui_openssl.c
-	echo To: `fgrep 'intr_signal;' crypto/ui/ui_openssl.c`
+	p To: `fgrep 'intr_signal;' crypto/ui/ui_openssl.c`
 
 	# Compile a version for the device...
 
 	PATH="${PLATFORM}"/Developer/usr/bin:$OPATH
 	export PATH
 
-	mkdir "${DISTDIR}"
+	# build release and debug versions
+	for dflag in "" -d; do
+		# # for now *always* build debug versions
+		# dflag=-d
+		x mkdir "${DISTDIR}${dflag}"
 
-	./config --openssldir="${DISTDIR}" ${ASSEMBLY}
+		p "# configuring: $dflag $DISTDIR $ASSEMBLY" 
+		x ./config ${dflag} --openssldir="${DISTDIR}${dflag}" ${ASSEMBLY}
 
-	perl -pi.bak \
-		-e "s#CC= cc#CC=${PLATFORM}/Developer/usr/bin/gcc# ;" \
-		-e "s#CFLAG= #CFLAG=-arch ${ARCH} -isysroot ${SDKPATH} # ;" \
-		-e "s#-arch i386#-arch ${ARCH}# ;" \
-		Makefile
+		perl -pi.bak \
+			-e "s#CC= cc#CC=${PLATFORM}/Developer/usr/bin/gcc# ;" \
+			-e "s#CFLAG= #CFLAG=-arch ${ARCH} -isysroot ${SDKPATH} # ;" \
+			-e "s#-arch i386#-arch ${ARCH}# ;" \
+			Makefile
 
-	case $LIBNAME in
-		${LIBNAME_SIMULATOR})
-			perl -pi.bak \
-				-e 'if (/LIBDEPS=/) { s/\)}";/\)} -L.";/; }' \
-					Makefile.shared
-			(cd apps; ln -s "${SDKPATH}"/usr/lib/crt1.10.5.o crt1.10.6.o);
-			(cd test; ln -s "${SDKPATH}"/usr/lib/crt1.10.5.o crt1.10.6.o);
-			;;
-	esac
+		case $LIBNAME in
+			${LIBNAME_SIMULATOR})
+				perl -pi.bak \
+					-e 'if (/LIBDEPS=/) { s/\)}";/\)} -L.";/; }' \
+						Makefile.shared
+				(cd apps; ln -s "${SDKPATH}"/usr/lib/crt1.10.5.o crt1.10.6.o);
+				(cd test; ln -s "${SDKPATH}"/usr/lib/crt1.10.5.o crt1.10.6.o);
+				;;
+		esac
 
-	# using && means the next command only runs if the previous commands all succeeded
-	# use -j (with # of CPUs) for parallel, fast build
-	echo "# Building clean first"
-	make -j `/usr/bin/hwprefs cpu_count` clean
-	echo "# Building libraries (only)"
-	make -j `/usr/bin/hwprefs cpu_count` build_libs && make install
-	BUILD_STATUS=$?
+		# use -j (with # of CPUs) for parallel, fast build
+		p "# Building clean first"
+		x make -j `/usr/bin/hwprefs cpu_count` clean
+		p "# Building libraries (only)"
+		# using && means the next command only runs if the previous commands all succeeded
+		x make -j `/usr/bin/hwprefs cpu_count` build_libs
+		BUILD_STATUS=$?
+		[ $BUILD_STATUS != 0 ] && break
+		p "# installing ${DISTDIR}${dflag}"
+		x make install
+	done
 	cd -
 }
 
@@ -64,20 +73,21 @@ extract_tarball() {
 	local tarball="$1"
 	local dir="${tarball%%.tar.gz}"
 	if [ -d "${dir}" ]; then
-		echo Removing previous tarball directory: ${dir}
+		p Removing previous tarball directory: ${dir}
 		/bin/rm -rf ${dir}
 	fi
-	echo Extracting tarball ${tarball}
-	tar zxf ${tarball}
+	p Extracting tarball ${tarball}
+	x tar zxf ${tarball}
 }
 
 clean_dir() {
 	local dir="$1"
 	if [ $BUILD_STATUS = 0 ]; then
-		echo Cleaning up "${DIR}"
+		p Cleaning up "${DIR}"
 		/bin/rm -rf ${DIR}
 	else
-		echo "Build failed ($BUILD_STATUS)"
+		p "Build failed ($BUILD_STATUS)"
+		p "Output is in $OUT"
 		exit 1
 	fi
 }
@@ -85,52 +95,21 @@ clean_dir() {
 latest_sdk() {
 	# set SDK to the last SDK in directory $1 (sorted lexically)
 	local dir="$1"
-	pushd "$dir"
+	x pushd "$dir"
 	local sdk_list=(*)
 	local sdk_count=${#sdk_list[*]}
 	local last_sdk_index=$sdk_count
 	((last_sdk_index--))
 	LATEST_SDK="${dir}/${sdk_list[$last_sdk_index]}"
-	popd
+	x popd
 }
 
-# use getopt to get command-line options
-args=`getopt d:t: $*`; errcode=$?; set -- $args
-
-if [ $errcode != 0 ]; then
-	echo <<EOF
-Usage: $0 [-d dir] [-t tb.tar.gz]
-	-d dir -- build already-extracted tarball in dir
-	-t tb.tar.gz -- use specified tarball
-You should specify **either** the tarball or the directory, but not both.	
-EOF
-	exit 1
-fi
-
-for i; do
-	echo "i is $i [$*]"
-	case "$i"
-	in
-		-d) echo "target_dir $2"; TARGET_DIR="$2"; shift;;
-		-t) echo "target $2"; TARGET="$2"; shift;;
-		--) shift; break;;
-	esac
-done
-
-if [ ${#TARGET} -gt 0 -a ${#TARGET_DIR} -gt 0 ]; then
-	echo "Only specify *either* the target directory or the tarball"
-	exit 1
-fi
-if [ ${#TARGET} = 0 -a ${#TARGET_DIR} = 0 ]; then
-	echo "Please specify either the target directory or the tarball"
-	exit 1
-fi
-
-if [ ${#TARGET} ]; then
-	TARGET_DIR="${TARGET%%.tar.gz}"
-fi
-
 setup_vars() {
+	# clean up old compiler output
+	rm -f /tmp/buildopenssl-*
+	# where verbose compiler output goes
+	OUT=`mktemp /tmp/build_openssl.XXXXXX` || exit 1
+
 	# use whatever name you want for the libraries
 	LIBNAME_DEVICE=device
 	LIBNAME_SIMULATOR=simulator
@@ -159,7 +138,52 @@ setup_vars() {
 
 }
 
+p() {
+	echo $* | tee -a $OUT 2>&1
+}
+
+x() {
+	$* >> $OUT 2>&1
+}
+
+# use getopt to get command-line options
+args=`getopt d:t: $*`; errcode=$?; set -- $args
+
+if [ $errcode != 0 ]; then
+	echo <<EOF
+Usage: $0 [-d dir] [-t tb.tar.gz]
+	-d dir -- build already-extracted tarball in dir
+	-t tb.tar.gz -- use specified tarball
+You should specify **either** the tarball or the directory, but not both.	
+EOF
+	exit 1
+fi
+
+for i; do
+	case "$i"
+	in
+		-d) TARGET_DIR="$2"; shift;;
+		-t) TARGET="$2"; shift;;
+		--) shift; break;;
+	esac
+done
+
+if [ ${#TARGET} -gt 0 -a ${#TARGET_DIR} -gt 0 ]; then
+	echo "Only specify *either* the target directory or the tarball"
+	exit 1
+fi
+if [ ${#TARGET} = 0 -a ${#TARGET_DIR} = 0 ]; then
+	echo "Please specify either the target directory or the tarball"
+	exit 1
+fi
+
+if [ ${#TARGET} ]; then
+	TARGET_DIR="${TARGET%%.tar.gz}"
+fi
+
 setup_vars
+
+p "# Compiler output is in $OUT"
 
 # build device library
 [ ${#TARGET} -gt 0 ] && extract_tarball "${TARGET}"
@@ -176,18 +200,24 @@ build_openssl "$TARGET_DIR" "${LIBNAME_SIMULATOR}" "$IOS_SIMULATOR_PLATFORM" "$I
 clean_dir "${TARGET%%.tar.gz}"
 
 # then, combine them into one..
-echo "Creating combined binary into directory 'dist'"
-[ -d dist ] && /bin/rm -rf dist
-mkdir dist
-(cd dist-${LIBNAME_DEVICE}; tar cf - . ) | (cd dist; tar xf -)
-
-for i in crypto ssl
-do
-	lipo \
-		-create dist-${LIBNAME_DEVICE}/lib/lib$i.a dist-${LIBNAME_SIMULATOR}/lib/lib$i.a \
-		-o dist/lib/lib$i.a
+# iterate over debug and non-debug versions
+for d in "" "-d"; do
+	# lipo both release and debug
+	[ -d dist$d ] && /bin/rm -rf dist$d
+	mkdir dist$d
+	# check for directories being there
+	[ ! -d dist-${LIBNAME_DEVICE}$d -o ! -d dist-${LIBNAME_SIMULATOR}$d ] && continue
+	p "# Creating combined binary into directory dist$d"
+	(cd dist-${LIBNAME_DEVICE}$d; tar cf - . ) | (cd dist$d; tar xf -) >> $OUT 2>&1
+	for i in crypto ssl; do
+		x lipo \
+			-create dist-${LIBNAME_DEVICE}$d/lib/lib$i.a dist-${LIBNAME_SIMULATOR}$d/lib/lib$i.a \
+			-o dist$d/lib/lib$i$d.a
+	done
 done
 
-/bin/rm -rf dist-${LIBNAME_SIMULATOR} dist-${LIBNAME_DEVICE}
+# /bin/rm -rf dist-${LIBNAME_SIMULATOR} dist-${LIBNAME_DEVICE}
+# /bin/rm -rf dist-${LIBNAME_SIMULATOR}-d dist-${LIBNAME_DEVICE}-d
 
-echo "Now package is ready in 'dist' directory'"
+p "Now package is ready in 'dist' directory'"
+p "Verbose output is in $OUT"
